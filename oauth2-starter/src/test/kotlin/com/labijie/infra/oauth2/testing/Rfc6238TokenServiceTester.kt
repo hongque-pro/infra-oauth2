cfe6f0b18323e1de903ee24a1e219cd9ca676d34
endink <sharping@outlook.com>
2020-10-29 23:42:39 +0800

    fix unit test

--- oauth2-starter/src/test/kotlin/com/labijie/infra/oauth2/testing/Rfc6238TokenServiceTester.kt
+++ oauth2-starter/src/test/kotlin/com/labijie/infra/oauth2/testing/Rfc6238TokenServiceTester.kt
@@ -1,17 +1,16 @@
 package com.labijie.infra.oauth2.testing
 
 import com.labijie.infra.security.Rfc6238TokenService
-import org.junit.Assert
-import org.junit.Test
-import java.io.Console
+import org.junit.jupiter.api.Assertions
 import java.util.*
+import kotlin.test.Test
 
 /**
  * Created with IntelliJ IDEA.
  * @author Anders Xiao
  * @date 2019-07-14
  */
-class Rfc6238TokenServiceTester {
+open class Rfc6238TokenServiceTester {
 
     private val rfc6238TokenService = Rfc6238TokenService()
     private val securityToken = UUID.randomUUID().toString().toByteArray()
@@ -21,25 +20,25 @@ class Rfc6238TokenServiceTester {
         repeat(100000){
             val modifier = UUID.randomUUID().toString()
             val code =  rfc6238TokenService.generateCodeString(securityToken.toString(Charsets.UTF_8),  modifier)
-            Assert.assertTrue(code.length == 6)
+            Assertions.assertTrue(code.length == 6)
             System.out.println(code)
             val valid = rfc6238TokenService.validateCodeString(code, securityToken.toString(Charsets.UTF_8), modifier)
-            Assert.assertTrue(valid)
+            Assertions.assertTrue(valid)
         }
     }
 
     @Test
     fun test(){
         val code = rfc6238TokenService.generateCode(securityToken)
-        Assert.assertEquals(true, rfc6238TokenService.validateCode(code, securityToken))
+        Assertions.assertEquals(true, rfc6238TokenService.validateCode(code, securityToken))
 
 
         val code2 = rfc6238TokenService.generateCode(securityToken)
-        Assert.assertEquals(false, rfc6238TokenService.validateCode(code2 + 2, securityToken))
+        Assertions.assertEquals(false, rfc6238TokenService.validateCode(code2 + 2, securityToken))
 
         val modifier = UUID.randomUUID().toString()
         val code3 = rfc6238TokenService.generateCode(securityToken, modifier)
-        Assert.assertEquals(true, rfc6238TokenService.validateCode(code3, securityToken, modifier))
-        Assert.assertEquals(false, rfc6238TokenService.validateCode(code3, securityToken, modifier + "ccc"))
+        Assertions.assertEquals(true, rfc6238TokenService.validateCode(code3, securityToken, modifier))
+        Assertions.assertEquals(false, rfc6238TokenService.validateCode(code3, securityToken, modifier + "ccc"))
     }
 }
