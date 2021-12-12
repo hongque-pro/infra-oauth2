1ba75ab4434e951fc0c56dbce814abe990cb57f5
endink <sharping@outlook.com>
2021-12-13 01:40:25 +0800

    fix dependencies version declare

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
     group = "com.labijie.infra"
-    version = "1.2.0"
+    version = "1.2.1"
 
     infra {
         useDefault {
--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -7,11 +7,11 @@
 object Versions {
     const val kotlin = "1.6.0"
     const val infraBom = "2.6.3"
-    const val infraOrm = "1.0.0"
-    const val infraPlugin = "1.0.6"
-    const val infraCommons = "2.2.0"
+    const val infraOrm = "1.0.1"
+    const val infraPlugin = "1.0.7"
+    const val infraCommons = "2.2.1"
     const val springSecurityOauth2 = "2.6.1"
     const val oauth2OidcSdk = "9.20"
-    const val infraCaching = "1.3.2"
+    const val infraCaching = "1.3.3"
     const val springAuthorizationServer = "0.2.1"
 }
