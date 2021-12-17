c7c5d11e068eaaf58521d6b43392837d7ccb9ef5
endink <sharping@outlook.com>
2021-12-17 19:26:51 +0800

    upgrade orm version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -7,7 +7,7 @@
 object Versions {
     const val kotlin = "1.6.0"
     const val infraBom = "2.6.4"
-    const val infraOrm = "1.0.1"
+    const val infraOrm = "1.0.2"
     const val infraPlugin = "1.0.11"
     const val infraCommons = "2.2.1"
     const val springSecurityOauth2 = "2.6.1"
