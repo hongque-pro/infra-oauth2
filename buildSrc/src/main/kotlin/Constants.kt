9db6d492e5a9bc07f8e2be614f2327d2d7baf603
endink <sharping@outlook.com>
2024-01-01 17:22:57 +0800

    upgrade caching kotlin

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -11,6 +11,6 @@ object Versions {
     const val infraPlugin = "2.0.2"
     const val infraCommons = "3.0.0"
     const val oauth2OidcSdk = "11.6"
-    const val infraCaching = "1.4.0"
+    const val infraCaching = "1.4.1"
     const val springAuthorizationServer = "0.2.1"
 }
