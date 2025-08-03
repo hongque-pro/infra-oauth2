deafa85c170d0c73c07bed2b766704bd131a798b
endink <sharping@outlook.com>
2025-08-03 16:30:41 +0800

    fix configuration-metadata

--- oauth2-authorization-server-starter/src/main/kotlin/com/labijie/infra/oauth2/configuration/JwtSettings.kt
+++ oauth2-authorization-server-starter/src/main/kotlin/com/labijie/infra/oauth2/configuration/JwtSettings.kt
@@ -1,11 +1,18 @@
 package com.labijie.infra.oauth2.configuration
 
+import org.springframework.boot.context.properties.NestedConfigurationProperty
+
 
 /**
  * Created with IntelliJ IDEA.
  * @author Anders Xiao
  * @date 2019-02-23
  */
-data class JwtSettings(var rsa: RSASettings = RSASettings())
+class JwtSettings {
+    @NestedConfigurationProperty
+    val rsa: RSASettings = RSASettings()
+}
 
-data class RSASettings(var privateKey: String = "", var publicKey: String = "")
+data class RSASettings(
+    var privateKey: String = "",
+    var publicKey: String = "")
