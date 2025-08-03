98b6338395ec573b88cb4e2b3f64cc8fb2467d83
endink <sharping@outlook.com>
2025-08-03 17:27:11 +0800

    add gen rsa from private key

--- oauth2-authorization-server-starter/src/main/kotlin/com/labijie/infra/oauth2/component/DefaultOAuth2ServerRSAKeyPair.kt
+++ oauth2-authorization-server-starter/src/main/kotlin/com/labijie/infra/oauth2/component/DefaultOAuth2ServerRSAKeyPair.kt
@@ -5,9 +5,14 @@ import com.labijie.infra.oauth2.RsaUtils
 import com.labijie.infra.oauth2.configuration.OAuth2ServerProperties
 import com.labijie.infra.utils.logger
 import com.nimbusds.jose.jwk.RSAKey
+import org.bouncycastle.jcajce.provider.asymmetric.RSA
+import java.security.KeyFactory
 import java.security.KeyPair
+import java.security.PublicKey
+import java.security.interfaces.RSAPrivateCrtKey
 import java.security.interfaces.RSAPrivateKey
 import java.security.interfaces.RSAPublicKey
+import java.security.spec.RSAPublicKeySpec
 import java.util.*
 
 /**
@@ -20,19 +25,30 @@ class DefaultOAuth2ServerRSAKeyPair(
 ) : IOAuth2ServerRSAKeyPair {
 
     private val rsaConfigured by lazy {
-        val default = serverProperties.token.jwt.rsa.privateKey.isBlank() || serverProperties.token.jwt.rsa.publicKey.isBlank()
         if(serverProperties.token.jwt.rsa.privateKey.isBlank()) {
             logger.warn("RSA private key is missing for oauth2 server.")
+            false
+        }else {
+            true
         }
-        if(serverProperties.token.jwt.rsa.publicKey.isBlank()) {
-            logger.warn("RSA public key is missing for oauth2 server.")
-        }
-
-        default
     }
 
 
     private var keyId = UUID.randomUUID().toString()
+
+    private fun loadPublicKeyFromPrivateKey(privateKey: RSAPrivateKey): RSAPublicKey? {
+        return try {
+            if (privateKey is RSAPrivateCrtKey) {
+                val keyFactory = KeyFactory.getInstance("RSA")
+                val spec = RSAPublicKeySpec(privateKey.modulus, privateKey.publicExponent)
+                keyFactory.generatePublic(spec) as? RSAPublicKey
+            }else null
+        }
+        catch (e: Throwable) {
+            logger.warn("Could not generate public key from rsa private key for oauth2 server", e)
+            null
+        }
+    }
 
     override fun isDefaultKeys(): Boolean {
         return rsaConfigured || secretsStore != null
@@ -46,7 +62,7 @@ class DefaultOAuth2ServerRSAKeyPair(
             val pub = RsaUtils.getPublicKey(secretsStore.getRsaPublicKey(serverProperties))
             val pri = RsaUtils.getPrivateKey(secretsStore.getRsaPrivateKey(serverProperties))
             KeyPair(pub, pri)
-        } else if (rsaConfigured) {
+        } else if (!rsaConfigured) {
             serverProperties.token.jwt.rsa.privateKey =
                 Base64.getEncoder().encodeToString(RsaUtils.defaultKeyPair.private.encoded)
             serverProperties.token.jwt.rsa.publicKey =
@@ -56,9 +72,12 @@ class DefaultOAuth2ServerRSAKeyPair(
             val privateKey =
                 OAuth2Utils.loadContent(serverProperties.token.jwt.rsa.privateKey, RsaUtils::getPrivateKey)
                     ?: throw IllegalArgumentException("${OAuth2ServerProperties.Companion.PRIVATE_KEY_PROPERTY_PATH} is an invalid private rsa key.")
-            val publicKey =
+            val publicKey = if(serverProperties.token.jwt.rsa.publicKey.isBlank()) {
+                loadPublicKeyFromPrivateKey(privateKey)
+            }else {
                 OAuth2Utils.loadContent(serverProperties.token.jwt.rsa.publicKey, RsaUtils::getPublicKey)
                     ?: throw IllegalArgumentException("${OAuth2ServerProperties.Companion.PUBLIC_KEY_PROPERTY_PATH} is an invalid public rsa key.")
+            }
             KeyPair(publicKey, privateKey)
         }
         val publicKey: RSAPublicKey = kp.public as RSAPublicKey
